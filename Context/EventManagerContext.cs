dac7c85c53cbbfb3d099c093a35b8f2fa214abe0
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-11-26 20:26:37 -0300

    fix: Remove method not use

--- Context/EventManagerContext.cs
+++ Context/EventManagerContext.cs
@@ -13,13 +13,5 @@ namespace EventManager.Context
         public EventManagerContext(DbContextOptions<EventManagerContext> options) : base(options)
         {
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Organizer>()
-                .HasMany(o => o.Events)
-                .WithOne(e => e.Organizer)
-                .HasForeignKey(e => e.OrganizerId);
-        }
     }
 }
